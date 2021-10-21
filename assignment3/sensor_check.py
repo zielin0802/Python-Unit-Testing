f46638f185f839dad3d46d4286b73f7e1bf236ee
Kevin Zielin <krzielin@health.ucsd.edu>
2021-10-20 17:50:35 -0700

    added docstrings to assignment3 modules

--- assignment3/sensor_check.py
+++ assignment3/sensor_check.py
@@ -1,16 +1,45 @@
 def temperature_in_range(temperature, min_temp=60, max_temp=90):
+    """Check if temperature is in acceptable range.
+
+    :param temperature: (int, float): temperature to check
+    :param min_temp: (int, float): minimum acceptable temperature
+    :param max_temp: (int, float): maximum acceptable temperature
+    :return bool:
+    """
     return min_temp <= temperature <= max_temp
 
+
 def humidity_in_range(humidity, min_humidity=25, max_humidity=40):
-    """Is humidity within the range  < 25 to 40> Yes : Pass , No : Fail"""
+    """Check if humidity is in acceptable range.
+
+    :param humidity: (int, float): humidity to check
+    :param min_humidity: (int, float): minimum acceptable humidity
+    :param max_humidity: (int, float): maximum acceptable humidity
+    :return bool:
+    """
     return min_humidity <= humidity <= max_humidity
 
+
 def seconds_in_range(seconds, min_seconds=10, max_seconds=20):
-    """Does the output readings reset within the range of 10 to 20 secs , Yes :  Pass, No : fail"""
+    """Check if seconds are in acceptable range.
+
+    :param seconds: (int, float): seconds to check
+    :param min_seconds: (int, float): minimum acceptable seconds
+    :param max_seconds: (int, float): maximum acceptable seconds
+    :return bool:
+    """
     return min_seconds <= seconds <= max_seconds
 
+
 def data_similar(cloud_data, local_data):
-    """Does the data displayed on thingspeak cloud as well showed on the RSPi local terminal similar < yes : Pass and NO Fail >"""
+    """Check if two data collections are the same.
+
+    :param cloud_data: (list, tuple): collection representing cloud data.
+    :param local_data: (list, tuple): collection representing local data.
+    :return bool:
+    :raises TypeError: if cloud_data or local_data not a list or tuple
+    :raises ValueError: if cloud_data and local_data are unequal in length
+    """
     if not isinstance(cloud_data, (list, tuple)):
         raise TypeError(f'Invalid type for cloud_data: {type(cloud_data)}')
     if not isinstance(local_data, (list, tuple)):
--- assignment3/test_sensor_check.py
+++ assignment3/test_sensor_check.py
@@ -1,15 +1,16 @@
-"""Units tests for test_sensor_check.py:
+"""Units tests for test_sensor_check.py.
 
-Practicing nosetest
+Practicing testing with nose.
 """
 
 import nose
-from nose.tools import assert_raises, assert_true, assert_false, eq_, ok_
+from nose.tools import assert_false, assert_raises, assert_true, eq_, ok_
 import unittest
 from sensor_check import data_similar, humidity_in_range, seconds_in_range, temperature_in_range
 
 
 def test_humidity_in_range_valid_inputs():
+    """Test for humidity_in_range(). Valid input parameters."""
     assert not humidity_in_range(24)
     assert not humidity_in_range(24.001)
     assert humidity_in_range(25)
@@ -20,10 +21,12 @@ def test_humidity_in_range_valid_inputs():
 
 
 def test_humid_in_range_invalid_inputs():
+    """Test for humidity_in_range(). Invalid input parameters."""
     assert_raises(TypeError, humidity_in_range, '40')
 
 
 def test_seconds_in_range_valid_inputs():
+    """Test for seconds_in_range(). Valid input parameters."""
     assert not seconds_in_range(9)
     assert not seconds_in_range(9.999999)
     assert seconds_in_range(10)
@@ -34,12 +37,14 @@ def test_seconds_in_range_valid_inputs():
 
 
 def test_seconds_in_range_invalid_inputs():
+    """Test for seconds_in_range(). Invalid input parameters."""
     assert_raises(TypeError, seconds_in_range, '15', 1, 10)
 
 
 class TestSensorCheck(unittest.TestCase):
-    """Test for temperature_in_range. Valid numeric inputs."""
+    """class definition for temperature_in_range() unit tests."""
     def test_temperature_in_range_valid_inputs(self):
+        """Test for temperature_in_range(). Valid inputs."""
         self.assertFalse(temperature_in_range(temperature=59.999))
         self.assertTrue(temperature_in_range(temperature=60))
         self.assertTrue(temperature_in_range(temperature=80.2))
@@ -47,11 +52,12 @@ class TestSensorCheck(unittest.TestCase):
         self.assertFalse(temperature_in_range(temperature=90.001))
 
     def test_temperature_in_range_invalid_inputs(self):
-        """Test for test_temperature in range. str input."""
+        """Test for temperature_in_range(). Invalid inputs."""
         assert_raises(TypeError, temperature_in_range, '50')
 
 
 def test_data_similar_valid_inputs():
+    """Test for data_similar(). Valid inputs."""
     ok_(data_similar([80, 30], (80, 30)))
     eq_(data_similar([80, 30], (80, 40)), False)
     assert not data_similar([10, 20], [10, 30])
@@ -59,7 +65,8 @@ def test_data_similar_valid_inputs():
     assert_false(data_similar([1, 2, 3], [1, 2, 4]))
 
 
-def test_data_milar_invalid_inputs():
+def test_data_similar_invalid_inputs():
+    """Test for data_similar(). Invalid inputs."""
     assert_raises(ValueError, data_similar, [1, 2, 3], [1, 2])
     assert_raises(TypeError, data_similar, 5, [5])
     assert_raises(TypeError, data_similar, [1, 2], '1, 2')
